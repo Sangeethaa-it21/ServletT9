f9b1ff21e8399e33dd2ef8a9586c2151b5686fd2
sangeethaa-it21 <sangeethaa.it21@kascsathy.ac.in>
2023-09-08 04:52:46 +0000

    ServletT9

---
+++ UserData.java
@@ -0,0 +1,14 @@
+package com.cslc.MainPackage;
+public class UserData {
+
+        public void storeUserCredentials (String user_name, String user_password) {
+                Connect con=new Connect();
+                con.getConnection();
+                String query="insert into test.user_credentials values (NULL, '"+user_name+"','"+user_p>
+                con.executeUpdateQuery (query);
+                con.closeConnection();
+        }
+        public void viewUserCredentials() {
+
+        }
+}
